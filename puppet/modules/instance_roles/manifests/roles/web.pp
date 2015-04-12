ef6d46ae1daecc7fc4c700b76af513faf5190adf
Oriol Martí <orimarti@gmail.com>
2015-04-12 17:03:17 +0200

    Added nginx and passenger to the web role

--- puppet/modules/instance_roles/manifests/roles/web.pp
+++ puppet/modules/instance_roles/manifests/roles/web.pp
@@ -1,3 +1,32 @@
 class instance_roles::roles::web {
+
   # Role for the web
+
+  ##############################################
+  # Nginx + Passenger installation and config
+  ##############################################
+  class { 'nginx':
+    package_source => 'passenger',
+    package_name => 'nginx-extras',
+    http_cfg_append => {
+      'passenger_root' => '/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini',
+      'passenger_ruby' => '/usr/bin/ruby',
+    }
+  }
+
+  # Nginx Virtualhost with passenger
+  nginx::resource::vhost {'default':
+    ensure      => 'present',
+    server_name => ['_'],
+    listen_port => 80,
+    vhost_cfg_append     => {
+      'passenger_enabled'  => 'on',
+      'passenger_ruby'     => '/usr/bin/ruby',
+      'rails_env'          => 'production',
+    },
+    www_root             => '/home/publify/publify/current/public',
+    use_default_location => false,
+    access_log           => '/var/log/nginx/blog_access.log',
+    error_log            => '/var/log/nginx/blog_error.log',
+  }
 }
